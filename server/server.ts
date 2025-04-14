513b454bca1d061eb83150fa5027aaccd7a56f1d
roshumba <roshumba.llewellyn@gmail.com>
2025-04-14 11:52:59 -0400

    fix(server.ts): add vercel conversion code

--- server/server.ts
+++ server/server.ts
@@ -7,7 +7,7 @@ import cors from 'cors';
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-
+import serverlessExpress from '@vendia/serverless-express';
 
 config();
 
@@ -43,4 +43,7 @@ app.listen(3000, () => {
   routes(app);
 });
 
-export default app;
+// export default app;
+// export default app;
+// Export wrapped handler
+exports.handler = serverlessExpress({ app });
